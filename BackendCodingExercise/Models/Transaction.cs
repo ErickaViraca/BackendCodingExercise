79f74c9fdb6fab00f2fd11f5f482b751cd01f823
ericka.viraca <ericka.viraca@truextend.com>
2021-07-10 08:40:03 -0400

    Add Transaction model.

---
+++ BackendCodingExercise/Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BillableTransactions.Models
+{
+    class Transaction
+    {
+        public DateTime date { get; set; }
+        public int amount { get; set; }
+        public string description { get; set; }
+        
+        //un-billed, billed, paid
+        public string paymentStatus { get; set; }
+    }
+}
